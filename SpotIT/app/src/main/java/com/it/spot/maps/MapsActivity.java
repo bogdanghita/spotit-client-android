d68dfe817739ff7ef282b1c4d8f5adcd76cdc050
Bogdan Ghita <bogdan.ghita93@gmail.com>
2016-03-19 17:22:30 +0200

    [Design][Login] - added icons and sidebar items - added sign out functionality - working on my phone. strage behavior on Teo's phone

--- SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
+++ SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
@@ -1,8 +1,19 @@
 package com.it.spot.maps;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,19 +21,37 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.it.spot.R;
+import com.it.spot.common.BaseActivity;
+import com.it.spot.common.Constants;
+import com.it.spot.common.ServiceManager;
+import com.it.spot.login.IdentityActivity;
+import com.it.spot.login.LoginActivity;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends IdentityActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
 	private GoogleMap mMap;
+
+	private ActionBarDrawerToggle mDrawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
+
+		// Configure toolbar
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		// Configure sidebar
+		DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		configureNavigationDrawer(drawerLayout, toolbar);
+
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
+
+
 	}
 
 
@@ -43,5 +72,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		LatLng sydney = new LatLng(-34, 151);
 		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+	}
+
+	private void configureNavigationDrawer(DrawerLayout mDrawerLayout, Toolbar toolbar) {
+
+		// Setting up Navigation Drawer
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+			@Override
+			public void onDrawerOpened(View v) {
+				super.onDrawerOpened(v);
+
+				invalidateOptionsMenu();
+				syncState();
+			}
+
+			@Override
+			public void onDrawerClosed(View v) {
+				super.onDrawerClosed(v);
+
+				invalidateOptionsMenu();
+				syncState();
+			}
+		};
+
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		mDrawerToggle.syncState();
+	}
+
+// -------------------------------------------------------------------------------------------------
+// LOGIN
+// -------------------------------------------------------------------------------------------------
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+		Log.d(Constants.APP + Constants.SIGN_IN, "onConnectionFailed()");
+	}
+
+	public void buttonSignOut(View view) {
+
+		Log.d(Constants.APP + Constants.MENU, "Button Sign out");
+
+		// Signing out from Google
+		Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+			@Override
+			public void onResult(Status status) {
+
+				Log.d("Identity logout status", status.toString());
+
+				// Clearing state of service manager
+				ServiceManager.getInstance().clear();
+
+				// Starting sign in activity
+				startSignInActivity();
+			}
+		});
+	}
+
+	private void startSignInActivity() {
+
+		Intent intent = new Intent(this, LoginActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		startActivity(intent);
 	}
 }
