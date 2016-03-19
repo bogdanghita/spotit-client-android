1f49418695a371cc488a463ee3a1be0cd967503a
Bogdan Ghita <17732325+bogdanghita@users.noreply.github.com>
2016-03-19 17:22:30 +0200

    [Design][Login] - added icons and sidebar items - added sign out functionality - working on my phone. strage behavior on Teo's phone

--- SpotIT/app/src/main/java/com/it/spot/common/Constants.java
+++ SpotIT/app/src/main/java/com/it/spot/common/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
 	public final static String APP = "APP-";
 	public final static String LIFECYCLE = "LIFEC-";
 	public final static String SIGN_IN = "SIGN_IN-";
+	public final static String MENU = "MENU-";
 
 	public final static int RC_SIGN_IN = 9001;
 }
