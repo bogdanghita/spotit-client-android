fdefc96a8dfce8abde2c9e05039ce226bd3bdd8e
Bogdan Ghita <bogdan.ghita93@gmail.com>
2016-03-19 19:56:43 +0200

    [Login] - solved issue on Android 6 - added permission check for google sign in - TODO: get token and perform authentification with Teo's server

--- SpotIT/app/build/generated/source/r/debug/com/google/android/gms/drive/R.java
+++ SpotIT/app/build/generated/source/r/debug/com/google/android/gms/drive/R.java
@@ -241,11 +241,11 @@ public final class R {
 		public static final int wallet_buy_button_place_holder = 0x7f07003a;
 	}
 	public static final class style {
-		public static final int Theme_IAPTheme = 0x7f0a00ed;
-		public static final int WalletFragmentDefaultButtonTextAppearance = 0x7f0a00f6;
-		public static final int WalletFragmentDefaultDetailsHeaderTextAppearance = 0x7f0a00f7;
-		public static final int WalletFragmentDefaultDetailsTextAppearance = 0x7f0a00f8;
-		public static final int WalletFragmentDefaultStyle = 0x7f0a00f9;
+		public static final int Theme_IAPTheme = 0x7f0a00eb;
+		public static final int WalletFragmentDefaultButtonTextAppearance = 0x7f0a00f4;
+		public static final int WalletFragmentDefaultDetailsHeaderTextAppearance = 0x7f0a00f5;
+		public static final int WalletFragmentDefaultDetailsTextAppearance = 0x7f0a00f6;
+		public static final int WalletFragmentDefaultStyle = 0x7f0a00f7;
 	}
 	public static final class styleable {
 		public static final int[] AdsAttrs = { 0x7f010026, 0x7f010027, 0x7f010028 };
