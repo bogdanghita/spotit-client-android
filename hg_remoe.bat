5467c16ebb07ac7c7e88c8d034c02d1da4154aa7
Bogdan Ghita <17732325+bogdanghita@users.noreply.github.com>
2016-03-22 12:21:51 +0200

    [SourceControl] - stopped traking of unnecessary files - added .bat script for automation

---
+++ hg_remoe.bat
@@ -0,0 +1,24 @@
+hg remove -f SpotIT/.gradle/**
+
+hg remove -f SpotIT/.gradle/**
+hg remove -f SpotIT/.idea/**
+
+hg remove -f SpotIT/app/build/**
+hg remove -f SpotIT/app/src/androidTest/**
+hg remove -f SpotIT/app/src/debug/**
+hg remove -f SpotIT/app/src/release/**
+hg remove -f SpotIT/app/src/test/**
+hg remove -f SpotIT/app/app.iml
+hg remove -f SpotIT/app/google-services.json
+hg remove -f SpotIT/app/proguard-rules.pro
+
+hg remove -f SpotIT/build/**
+hg remove -f SpotIT/gradle/**
+
+hg remove -f SpotIT/build.gradle
+hg remove -f SpotIT/gradle.properties
+hg remove -f SpotIT/gradlew
+hg remove -f SpotIT/gradlew.bat
+hg remove -f SpotIT/local.properties
+hg remove -f SpotIT/settings.gradle
+hg remove -f SpotIT/SpotIT.iml
