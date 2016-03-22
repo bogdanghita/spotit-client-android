db2714a4e96839f23110ea45a7eb80d4216fedb7
Bogdan Ghita <bogdan.ghita93@gmail.com>
2016-03-22 12:31:35 +0200

    [SourceControl] - stopped traking of unnecessary files - added .bat script for automation

---
+++ hg_forget.bat
@@ -0,0 +1,24 @@
+hg forget SpotIT/.gradle/**
+
+hg forget SpotIT/.gradle/**
+hg forget SpotIT/.idea/**
+
+hg forget SpotIT/app/build/**
+hg forget SpotIT/app/src/androidTest/**
+hg forget SpotIT/app/src/debug/**
+hg forget SpotIT/app/src/release/**
+hg forget SpotIT/app/src/test/**
+hg forget SpotIT/app/app.iml
+hg forget SpotIT/app/google-services.json
+hg forget SpotIT/app/proguard-rules.pro
+
+hg forget SpotIT/build/**
+hg forget SpotIT/gradle/**
+
+hg forget SpotIT/build.gradle
+hg forget SpotIT/gradle.properties
+hg forget SpotIT/gradlew
+hg forget SpotIT/gradlew.bat
+hg forget SpotIT/local.properties
+hg forget SpotIT/settings.gradle
+hg forget SpotIT/SpotIT.iml
