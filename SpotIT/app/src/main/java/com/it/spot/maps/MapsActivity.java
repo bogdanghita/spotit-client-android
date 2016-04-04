e84b5c12bb569c9f9045df59c7632847d8e104b8
teo721 <teo721@localhost>
2016-04-04 17:45:59 +0300

    [Design] report spots: normal alert with woth buttons with shadow and stroke

--- SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
+++ SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
@@ -645,14 +645,27 @@ public class MapsActivity extends IdentityActivity implements OnMapReadyCallback
 	}
 
 	public void buttonOpenParkingStateOptionsV2(View v) {
-		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3b3b58"));
-		colorDrawable.setAlpha(200);
-//		Dialog alertDialog = new Dialog(this);
-		Dialog alertDialog = new Dialog(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+
+		//Lasati chestiile comentate in caz ca vom modifica in fullscreen
+
+		//Normal alert
+//		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffffff"));
+		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#f9f9f9"));
+		Dialog alertDialog = new Dialog(this);
 		alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		alertDialog.setContentView(R.layout.report_parking_spot_layout);
+		alertDialog.setContentView(R.layout.report_parking_spot_layout2);
 		alertDialog.getWindow().setBackgroundDrawable(colorDrawable);
 		alertDialog.show();
+
+		//Fullscreen alert
+//		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3b3b58"));
+//		colorDrawable.setAlpha(200);
+//		Dialog alertDialog = new Dialog(this);
+//		Dialog alertDialog = new Dialog(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+//		alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//		alertDialog.setContentView(R.layout.report_parking_spot_layout2);
+//		alertDialog.getWindow().setBackgroundDrawable(colorDrawable);
+//		alertDialog.show();
 	}
 
 	public void buttonReportParkingState(View v) {
