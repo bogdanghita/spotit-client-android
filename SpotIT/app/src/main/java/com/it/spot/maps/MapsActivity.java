de59577247545309e17fb9d8eee3b32f5da4417f
Nicolae-Teodor Pavel (Teo) <17949718+nicolae-teodor-pavel@users.noreply.github.com>
2016-04-09 12:55:11 +0300

    [Design] Animation for report state - needs a some factoring

--- SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
+++ SpotIT/app/src/main/java/com/it/spot/maps/MapsActivity.java
@@ -1,6 +1,11 @@
 package com.it.spot.maps;
 
 import android.Manifest;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -10,6 +15,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -22,7 +28,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -65,6 +74,7 @@ import com.it.spot.identity.TokenRequestEventListener;
 import com.it.spot.identity.UserInfo;
 import com.it.spot.services.PolygonUI;
 import com.it.spot.threading.Event;
+import com.ogaclejapan.arclayout.ArcLayout;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -104,6 +114,10 @@ public class MapsActivity extends IdentityActivity implements OnMapReadyCallback
 
 	private Dialog mReportParkingStateDialog;
 
+	View rootLayout;
+	ClipRevealFrame menuLayout;
+	ArcLayout arcLayout;
+	View centerItem;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -138,6 +152,10 @@ public class MapsActivity extends IdentityActivity implements OnMapReadyCallback
 
 		locationRouteService.loadSavedSpot();
 		toggleSaveSpotButton();
+
+		rootLayout = findViewById(R.id.drawer_layout);
+		menuLayout = (ClipRevealFrame) findViewById(R.id.menu_layout);
+		arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
 
 	}
 
@@ -748,21 +766,140 @@ public class MapsActivity extends IdentityActivity implements OnMapReadyCallback
 //		mReportParkingStateDialog.show();
 
 //		//Fullscreen alert
-		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3b3b58"));
-		colorDrawable.setAlpha(230);
-		mReportParkingStateDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
-		mReportParkingStateDialog.setContentView(R.layout.report_parking_spot_layout);
-		mReportParkingStateDialog.getWindow().setBackgroundDrawable(colorDrawable);
-		mReportParkingStateDialog.show();
+//		ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3b3b58"));
+//		colorDrawable.setAlpha(0);
+//		mReportParkingStateDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+//		mReportParkingStateDialog.setContentView(R.layout.report_parking_spot_layout);
+//		mReportParkingStateDialog.getWindow().setBackgroundDrawable(colorDrawable);
+//		mReportParkingStateDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+//			@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+//			@Override
+//			public void onShow(DialogInterface dialog) {
+//				View view = mReportParkingStateDialog.findViewById(android.R.id.content);
+//				int centerX = view.getWidth();
+//				int centerY = view.getHeight();
+//				// TODO Get startRadius from FAB
+//				// TODO Also translate animate FAB to center of screen?
+//				float startRadius = 20;
+//				float endRadius = (float) Math.hypot(view.getWidth(), view.getHeight());
+//				Animator animator = ViewAnimationUtils.createCircularReveal(view, centerX, centerY, startRadius, endRadius);
+//				animator.setDuration(1000);
+//				animator.start();
+//
+//			}
+//		});
+//		mReportParkingStateDialog.show();
+		onFabClick(v);
+
+	}
+	View mView;
+	private void onFabClick(View v) {
+		mView = v;
+		int x = (v.getLeft() + v.getRight()) / 2;
+		int y = (v.getTop() + v.getBottom()) / 2;
+		float radiusOfFab = 1f * v.getWidth() / 2f;
+		float radiusFromFabToRoot = (float) Math.hypot(
+				Math.max(x, rootLayout.getWidth() - x),
+				Math.max(y, rootLayout.getHeight() - y));
+
+		if (v.isSelected()) {
+			hideMenu(x, y, radiusFromFabToRoot, 0);
+		} else {
+			showMenu(x, y, 0, radiusFromFabToRoot);
+		}
+		v.setSelected(!v.isSelected());
+	}
+
+	private void showMenu(int cx, int cy, float startRadius, float endRadius) {
+		menuLayout.setVisibility(View.VISIBLE);
+		FloatingActionButton p = (FloatingActionButton) findViewById(R.id.fab_state_v2);
+		p.setVisibility(View.INVISIBLE);
+		FloatingActionButton c = (FloatingActionButton) findViewById(R.id.my_location);
+		c.setVisibility(View.INVISIBLE);
+
+		List<Animator> animList = new ArrayList<>();
+
+		Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+		revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+		revealAnim.setDuration(500);
+
+		animList.add(revealAnim);
+
+
+		AnimatorSet animSet = new AnimatorSet();
+		animSet.playSequentially(animList);
+		animSet.start();
+	}
+
+	private void hideMenu(int cx, int cy, float startRadius, float endRadius) {
+		List<Animator> animList = new ArrayList<>();
+
+
+
+		Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+		revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+		revealAnim.setDuration(200);
+		revealAnim.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				menuLayout.setVisibility(View.INVISIBLE);
+				FloatingActionButton p = (FloatingActionButton) findViewById(R.id.fab_state_v2);
+				p.setVisibility(View.VISIBLE);
+				FloatingActionButton c = (FloatingActionButton) findViewById(R.id.my_location);
+				c.setVisibility(View.VISIBLE);
+			}
+		});
+
+		animList.add(revealAnim);
+
+		AnimatorSet animSet = new AnimatorSet();
+		animSet.playSequentially(animList);
+		animSet.start();
 
 	}
 
-	public void buttonReportParkingState(View v) {
 
+	private Animator createCircularReveal(final ClipRevealFrame view, int x, int y, float startRadius,
+										  float endRadius) {
+		final Animator reveal;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			reveal = ViewAnimationUtils.createCircularReveal(view, x, y, startRadius, endRadius);
+		} else {
+			view.setClipOutLines(true);
+			view.setClipCenter(x, y);
+			reveal = ObjectAnimator.ofFloat(view, "ClipRadius", startRadius, endRadius);
+			reveal.addListener(new Animator.AnimatorListener() {
+				@Override
+				public void onAnimationStart(Animator animation) {
+
+				}
+
+				@Override
+				public void onAnimationEnd(Animator animation) {
+					view.setClipOutLines(false);
+				}
+
+				@Override
+				public void onAnimationCancel(Animator animation) {
+
+				}
+
+				@Override
+				public void onAnimationRepeat(Animator animation) {
+
+				}
+			});
+		}
+		return reveal;
+	}
+
+	public void buttonReportParkingState(View v) {
+		onFabClick(mView);
 		int buttonId = v.getId();
 
 		// Dismiss dialog
-		mReportParkingStateDialog.dismiss();
+		//mReportParkingStateDialog.dismiss();
 
 		findViewById(R.id.fab_free_2).setVisibility(View.GONE);
 		findViewById(R.id.fab_moderate_2).setVisibility(View.GONE);
