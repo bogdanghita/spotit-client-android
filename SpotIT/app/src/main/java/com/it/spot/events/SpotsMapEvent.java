6e0914eab110617f9b1ca54c0ca28116eb9a8849
Bogdan Ghita <bogdan.ghita93@gmail.com>
2016-04-24 21:13:30 +0300

    [SPOTS] - Spots update logic done

--- SpotIT/app/src/main/java/com/it/spot/events/SpotsMapEvent.java
+++ SpotIT/app/src/main/java/com/it/spot/events/SpotsMapEvent.java
@@ -1,11 +1,27 @@
 package com.it.spot.events;
 
+import com.it.spot.services.PolygonUI;
+
+import java.util.List;
+
 /**
  * Created by Bogdan on 19/04/2016.
  */
 public class SpotsMapEvent extends BaseEvent {
 
-	public SpotsMapEvent() {
+	private List<PolygonUI> polygons;
+
+	public SpotsMapEvent(List<PolygonUI> polygons) {
 		super(EventManager.EventType.MAP_LISTENER);
+
+		this.polygons = polygons;
+	}
+
+	public List<PolygonUI> getPolygons() {
+		return polygons;
+	}
+
+	public void setPolygons(List<PolygonUI> polygons) {
+		this.polygons = polygons;
 	}
 }
