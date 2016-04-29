b80923f7d39c9bd1ed848a64bb11e0551a80625a
teo721 <teo721@localhost>
2016-04-29 16:50:54 +0300

    [Design][Intro] Title , Image, description

--- SpotIT/app/src/main/java/com/it/spot/intro/IntroActivity.java
+++ SpotIT/app/src/main/java/com/it/spot/intro/IntroActivity.java
@@ -1,56 +1,58 @@
 package com.it.spot.intro;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.it.spot.R;
 import com.it.spot.maps.main.MapsActivity;
 
 public class IntroActivity extends AppIntro {
-
+	private String directions = "directions.png";
+	private String reportSpot = "report_spot.png";
 	@Override
 	public void init(Bundle savedInstanceState) {
 
 		// Add your slide's fragments here.
 		// AppIntro will automatically generate the dots indicator and buttons.
-		addSlide(SampleSlide.newInstance(R.layout.intro_spots_slide));
-		addSlide(SampleSlide.newInstance(R.layout.intro_colors_slide));
-		addSlide(SampleSlide.newInstance(R.layout.intro_report_spot_slide));
-		addSlide(SampleSlide.newInstance(R.layout.intro_save_location_slide));
-		addSlide(SampleSlide.newInstance(R.layout.intro_directions_slide));
-//		addSlide(second_fragment);
-//		addSlide(third_fragment);
-//		addSlide(fourth_fragment);
-//
-//		// Instead of fragments, you can also use our default slide
-//		// Just set a title, description, background and image. AppIntro will do the rest.
-//		addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+		addSlide(SampleSlide.newInstance(R.layout.intro_spots_slide, directions));
+		addSlide(SampleSlide.newInstance(R.layout.intro_colors_slide, reportSpot));
+		addSlide(SampleSlide.newInstance(R.layout.intro_report_spot_slide, reportSpot));
+		addSlide(SampleSlide.newInstance(R.layout.intro_save_location_slide,directions));
+		addSlide(SampleSlide.newInstance(R.layout.intro_directions_slide, directions));
 
-		// OPTIONAL METHODS
-		// Override bar/separator color.
-		//setBarColor(Color.parseColor("#3F51B5"));
-		//setSeparatorColor(Color.parseColor("#2196F3"));
 
-		// Hide Skip/Done button.
-//		showSkipButton(false);
-//		//setProgressButtonEnabled(false);
-//
+		showSkipButton(false);
 		setBarColor(Color.parseColor("#f9f9f9"));
 		setSeparatorColor(Color.parseColor("#ff4e5c"));
 		setIndicatorColor(Color.parseColor("#3b3b58"), Color.parseColor("#252545"));
-
-		View view = findViewById(com.github.paolorotolo.appintro.R.id.next);
-		view.setBackgroundColor(Color.parseColor("#f9f9f9"));
-		View view2 = findViewById(com.github.paolorotolo.appintro.R.id.done);
-		view2.setBackgroundColor(Color.parseColor("#f9f9f9"));
+		setDoneText("Get Started");
+		setColorDoneText(Color.parseColor("#ff4e5c"));
+		setNextArrowColor(Color.parseColor("#ff4e5c"));
 
 	}
+	public void setNextArrowColor(@ColorInt final int color) {
+		ImageButton nextButton = (ImageButton) findViewById(R.id.next);
+		nextButton.setColorFilter(color);
+	}
 
+	public void setDoneArrowColor(@ColorInt final int color) {
+		Button doneButton = (Button) findViewById(R.id.done);
+//		nextButton.setColorFilter(color);
+	}
 	@Override
 	public void onSkipPressed() {
 
