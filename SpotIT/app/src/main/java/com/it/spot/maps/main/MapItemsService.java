fabfd7e3be69238441435839a892fc87e9422a37
claudiu_mihai_toma <13257303+claudiu-mihai-toma@users.noreply.github.com>
2016-04-29 11:52:46 +0300

    [Address][Loading] - Changed the loading address to be event based. - Must change loading layout and add events for directions and duration.

--- SpotIT/app/src/main/java/com/it/spot/maps/main/MapItemsService.java
+++ SpotIT/app/src/main/java/com/it/spot/maps/main/MapItemsService.java
@@ -2,11 +2,9 @@ package com.it.spot.maps.main;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
+import com.it.spot.R;
 import com.it.spot.common.Constants;
 import com.it.spot.common.ServiceManager;
 import com.it.spot.events.CameraChangeEvent;
@@ -18,10 +16,11 @@ import com.it.spot.events.RemoveMarkerEvent;
 import com.it.spot.events.RemoveRouteEvent;
 import com.it.spot.events.SetMarkerEvent;
 import com.it.spot.events.SpotsMapEvent;
-import com.it.spot.services.FileService;
 import com.it.spot.maps.directions.RouteData;
-import com.it.spot.services.PolygonUI;
+import com.it.spot.services.FileService;
 import com.it.spot.threading.Event;
+import com.it.spot.threading.StateMonitorListener;
+import com.it.spot.threading.StateMonitorThread;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -29,29 +28,73 @@ import java.util.List;
 /**
  * Created by Bogdan on 22/04/2016.
  */
-public class MapItemsService extends MapEventListener {
+public class MapItemsService extends MapEventListener implements StateMonitorListener {
 
 	public enum MarkerType {SAVED_SPOT, DESTINATION, NONE}
 
 	private MapItemsManager mMapItemsManager;
 	private FileService mFileService;
 
+	private MapItemsProvider mMapItemsProvider;
+	private UiController mUiController;
 	private UiItemsController mUiItemsController;
 
 	private RouteLogic mRouteLogic;
 	private MarkerLogic mMarkerLogic;
 	private SpotsLogic mSpotsLogic;
 
+	private Event mAddressEvent;
+
 	public MapItemsService(Context context, MapItemsProvider mapItemsProvider, UiController uiController) {
 
 		mFileService = new FileService(context);
 		mMapItemsManager = ServiceManager.getInstance().getMapItemsManager();
 
+		mMapItemsProvider = mapItemsProvider;
+		mUiController = uiController;
 		mUiItemsController = new UiItemsController(context, mapItemsProvider, uiController);
 
 		mRouteLogic = new RouteLogic(mapItemsProvider, uiController);
 		mMarkerLogic = new MarkerLogic(mapItemsProvider, uiController, mUiItemsController);
 		mSpotsLogic = new SpotsLogic(mapItemsProvider, uiController);
+	}
+
+	void startLoading() {
+		mUiController.doRunOnUiThread(
+				new Runnable() {
+					@Override
+					public void run() {
+						// Loading destination info animation.
+						mMapItemsProvider.getView(R.id.location_address).setVisibility(View.GONE);
+						mMapItemsProvider.getView(R.id.loading_address).setVisibility(View.VISIBLE);
+					}
+				}
+		);
+	}
+
+	void stopLoading() {
+		mUiController.doRunOnUiThread(
+				new Runnable() {
+					@Override
+					public void run() {
+						// Stop loading destination info animation.
+						mMapItemsProvider.getView(R.id.loading_address).setVisibility(View.GONE);
+						mMapItemsProvider.getView(R.id.location_address).setVisibility(View.VISIBLE);
+					}
+				}
+		);
+	}
+
+	void startStateMonitor() {
+		mAddressEvent = new Event();
+		List<Event> eventList = new LinkedList<>();
+		eventList.add(mAddressEvent);
+		new StateMonitorThread(this, eventList).start();
+	}
+
+	@Override
+	public void notifyStateReady() {
+		stopLoading();
 	}
 
 	@Override
@@ -92,9 +135,17 @@ public class MapItemsService extends MapEventListener {
 
 		mRouteLogic.clearDirections();
 
+		// Start the loading animation.
+		startLoading();
+		// StateMonitor that awaits address, duration and directions.
+		startStateMonitor();
+
+		// Draws marker and obtains destination address.
+		mMarkerLogic.setAddressEvent(mAddressEvent);
 		mMarkerLogic.drawMarker();
 
-		// Perform call to get directions. When result is ready, directions will be populated
+		// Perform call to get directions, distance and duration.
+		// When result is ready, directions will be populated.
 		mRouteLogic.populateDirections();
 	}
 
