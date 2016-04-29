56e372a758d58a2cf2256f5663bfae063bace782
Nicolae-Teodor Pavel (Teo) <17949718+nicolae-teodor-pavel@users.noreply.github.com>
2016-04-29 16:50:54 +0300

    [Design][Intro] Title , Image, description

--- SpotIT/app/src/main/java/com/it/spot/intro/SampleSlide.java
+++ SpotIT/app/src/main/java/com/it/spot/intro/SampleSlide.java
@@ -3,28 +3,41 @@ package com.it.spot.intro;
 /**
  * Created by teo on 29.04.2016.
  */
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.it.spot.R;
+import com.squareup.picasso.Picasso;
+
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+import jp.wasabeef.picasso.transformations.CropSquareTransformation;
+import jp.wasabeef.picasso.transformations.CropTransformation;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 public class SampleSlide extends Fragment {
 
     private static final String ARG_LAYOUT_RES_ID = "layoutResId";
-
-    public static SampleSlide newInstance(int layoutResId) {
+    private static final String ARG_IMAGE_RES_NAME = "imageResName";
+    public static SampleSlide newInstance(int layoutResId,String imageName) {
         SampleSlide sampleSlide = new SampleSlide();
 
         Bundle args = new Bundle();
         args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
+        args.putString(ARG_IMAGE_RES_NAME, imageName);
         sampleSlide.setArguments(args);
 
         return sampleSlide;
     }
 
     private int layoutResId;
+    private String imageName;
 
     public SampleSlide() {}
 
@@ -32,14 +45,25 @@ public class SampleSlide extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID))
+        if(getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID)) {
             layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+            imageName = getArguments().getString(ARG_IMAGE_RES_NAME);
+        }
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(layoutResId, container, false);
+        View view = inflater.inflate(layoutResId, container, false);
+        ImageView img = (ImageView) view.findViewById(R.id.imageView);
+        int h = img.getHeight();
+        int w = img.getHeight();
+        Context c = getActivity().getApplicationContext();
+        Picasso.with(c).load("file:///android_asset/intro/" + imageName)
+                .transform(new CropSquareTransformation())
+                .into(img);
+        return view;
+// return inflater.inflate(layoutResId, container, false);
     }
 
 }
