d788efdfdf6d73b5f77c1622addbd388f724fd59
Nicolae-Teodor Pavel (Teo) <17949718+nicolae-teodor-pavel@users.noreply.github.com>
2016-04-29 15:03:17 +0300

    [Design][Report Spot Dialog] modify the circle size an position [Design][Intro] slides for intro with title only

---
+++ SpotIT/app/src/main/java/com/it/spot/intro/SpotsSlide.java
@@ -0,0 +1,23 @@
+package com.it.spot.intro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.it.spot.R;
+
+/**
+ * Created by teo on 29.04.2016.
+ */
+public class SpotsSlide extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_intro, container, false);
+    }
+
+}
